404c77a92bb9d333966cf6ae7afebc8013ddfb42
IDHEMEL <elmehdiouajjani@gmail.com>
2022-05-18 09:55:25 +0100

    commit

---
+++ 3-Jpa/src/test/java/ma/enset/jpaactivitepratique/JpaActivitePratiqueApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.jpaactivitepratique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaActivitePratiqueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
